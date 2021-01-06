a67e362679eb30c70903ba3f223d7ec0e926c099
Wesley Silva <wesleysa95@gmail.com.br>
2021-01-06 16:43:42 -0300

    Integrando a Aplicação ao Banco de Dados

--- 03_RestWithASPNET/RestWithASPNET/RestWithASPNET/Startup.cs
+++ 03_RestWithASPNET/RestWithASPNET/RestWithASPNET/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model.Context;
 using RestWithASPNET.Services;
 using RestWithASPNET.Services.Implementations;
 using System;
@@ -25,6 +27,9 @@ namespace RestWithASPNET {
     public void ConfigureServices(IServiceCollection services) {
 
       services.AddControllers();
+
+      var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+      services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
       //Dependecy injection
       services.AddScoped<IPersonService, PersonServiceImplementation>();
